7055ce79bb3a0ac1591db6714d93bf38028890b7
vipin <vipin@VipinKaushal>
2025-01-04 17:09:21 +0530

    Test

---
+++ AutomationProject/src/demo/demo.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class demo {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
---
+++ AutomationProject/src/demo/shoppersstack.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class shoppersstack {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.shoppersstack.com/");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
